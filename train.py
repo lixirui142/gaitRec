70466a4d63cc42365cb12d353c0605e43be3bddc
lixirui142 <936434653@qq.com>
2021-05-02 00:50:23 +0800

    t

--- train.py
+++ train.py
@@ -120,6 +120,7 @@ if __name__ == '__main__':
 	for i in range(3):
 		test_rank1[i].append(rank_one[i])
 
+
 	for epoch in range(args.epoch):
 		proc.adjust_lr(natural_decay(epoch))
 		print("Epoch %d" % epoch)
